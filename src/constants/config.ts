878e4581295689b2b67065b88730ec7991194f2c
Treay 1 Dev (MB) <treaty1tech@gmail.com>
2022-03-12 16:34:57 -0600

    feat(lang):add ojibwe language lists and config

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,18 +1,21 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'YourLanguageHere', // This changes the display name for your language
+  language: 'Ojibwe', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this Wordle!
-  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
-  wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
+  author:
+    'Treaty 1 Technologies Inc, | Manitoba Indigenous Cultural Education Centre Inc.', // Put your name here so people know who made this Wordle!
+  authorWebsite: 'https://treaty1tech.ca | https://www.micec.com', // Put a link to your website or social media here
+  wordListSource: "Ojibwe People's Dictionary", // Describe the source material for your words here
+  wordListSourceLink: 'https://ojibwe.lib.umn.edu/', // Put a link to the source material for your words here
+  apiKey: '', // key for where the wordlist and definitions are.
+  sheetUrl: '', //url for masterList
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
-  googleAnalytics: '', // You can use this if you use Google Analytics
+  googleAnalytics: 'UA-222149648-1', // You can use this if you use Google Analytics
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
   startDate: 'January 1, 2022 00:00:00', // what date and time to start your game from
   defaultLang: 'en', // the default interface language
-  availableLangs: ['en', 'es'], // the options available to the user for translation languages
+  availableLangs: ['en'], // the options available to the user for translation languages
 }
